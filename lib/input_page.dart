e363d01570fff1c4b8e6a198984e8154f43ea042
ElieBamunoba <eliebamunoba@gmail.com>
2022-06-14 22:26:16 +0300

    themes

---
+++ lib/input_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
